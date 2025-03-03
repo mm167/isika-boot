53ae6982cbfd89cf9cf05f92f1f7562e13079d61
doum167 <user20201901@gmail.com>
2025-03-03 11:32:38 +0100

    Initial commit

---
+++ src/main/java/com/formation/controller/TutorialController.java
@@ -0,0 +1,119 @@
+package com.formation.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.model.Tutorial;
+import com.formation.repository.TutorialRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api")
+public class TutorialController {
+
+  @Autowired
+  TutorialRepository tutorialRepository;
+
+  @GetMapping("/tutorials")
+  public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+    try {
+      List<Tutorial> tutorials = tutorialRepository.findAll();
+
+      if (tutorials.isEmpty()) {
+        return ResponseEntity.notFound().build();
+      }
+
+      return ResponseEntity.ok(tutorials);
+    } catch (Exception e) {
+      return ResponseEntity.status(500).build();
+    }
+  }
+
+  @GetMapping("/tutorials/{id}")
+  public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
+    try {
+      Tutorial tutorial = tutorialRepository.findById(id).get();
+
+      if (tutorial == null) {
+        return ResponseEntity.notFound().build();
+      }
+
+      return ResponseEntity.ok(tutorial);
+    } catch (Exception e) {
+      return ResponseEntity.status(500).build();
+    } 
+  }
+
+  @PostMapping("/tutorials")
+  public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
+    try {
+      Tutorial _tutorial = tutorialRepository
+          .save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+      return ResponseEntity.ok(_tutorial);
+    } catch (Exception e) {
+      return ResponseEntity.status(500).build();
+    }
+  }
+
+  @PutMapping("/tutorials/{id}")
+  public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
+    try {
+      Tutorial _tutorial = tutorialRepository.findById(id).get();
+      _tutorial.setTitle(tutorial.getTitle());
+      _tutorial.setDescription(tutorial.getDescription());
+      _tutorial.setPublished(tutorial.isPublished());
+      return ResponseEntity.ok(tutorialRepository.save(_tutorial));
+    } catch (Exception e) {
+      return ResponseEntity.status(500).build();
+    }
+  }
+
+  @DeleteMapping("/tutorials/{id}")
+  public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
+    try {
+      tutorialRepository.deleteById(id);
+      return ResponseEntity.ok().build();
+    } catch (Exception e) {
+      return ResponseEntity.status(500).build();
+    }
+  }
+
+  @DeleteMapping("/tutorials")
+  public ResponseEntity<HttpStatus> deleteAllTutorials() {
+    try {
+      tutorialRepository.deleteAll();
+      return ResponseEntity.ok().build();
+    } catch (Exception e) {
+      return ResponseEntity.status(500).build();
+    }
+  }
+
+  @GetMapping("/tutorials/published")
+  public ResponseEntity<List<Tutorial>> findByPublished() {
+    try {
+      List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+
+      if (tutorials.isEmpty()) {
+        return ResponseEntity.notFound().build();
+      }
+
+      return ResponseEntity.ok(tutorials);
+    } catch (Exception e) {
+      return ResponseEntity.status(500).build();
+    }
+  }
+    
+}
